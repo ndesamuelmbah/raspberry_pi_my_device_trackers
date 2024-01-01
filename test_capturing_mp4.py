9809407bb30ec490000ab4bdecb78b560e44d508
Samuel Nde <ndesamuelmbah@gmail.com>
2024-01-01 07:35:18 -0500

    Test Capturing mp4 with sound.

--- test_capturing_mp4.py
+++ test_capturing_mp4.py
@@ -3,22 +3,24 @@ import time
 from datetime import datetime, timedelta
 
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import H264Encoder, JpegEncoder, MJPEGEncoder
 from picamera2.outputs import FfmpegOutput
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 print(f'{datetime.now()}: Video configuration:')
-encoder = H264Encoder(10000000)
-print(f'{datetime.now()} Set up the time: ')
-output = FfmpegOutput('test.mp4', audio=False)
+encoders = {"H264Encoder": H264Encoder(10000000), "JpegEncoder": JpegEncoder(10000000), "MJPEGEncoder": MJPEGEncoder(10000000)}
+for name, encoder in encoders.items():
+    encoder = H264Encoder(10000000)
+    print(f'{name} {datetime.now()} Set up the time: ')
+    output = FfmpegOutput('test_{name}.mp4', audio=False)
 
-print(f'{datetime.now()} Set up the output: ')
+    print(f'{name} {datetime.now()} Set up the output: ')
 
-picam2.start_recording(encoder, output)
-print(f'{datetime.now()} Started Recording: ')
-time.sleep(10)
-print(f'{datetime.now()} Recording for 10 seconds:')
-picam2.stop_recording()
-print(f'{datetime.now()} Recording for 10 seconds AND Done:')
+    picam2.start_recording(encoder, output)
+    print(f'{name} {datetime.now()} Started Recording: ')
+    time.sleep(10)
+    print(f'{name} {datetime.now()} Recording for 10 seconds:')
+    picam2.stop_recording()
+    print(f'{name} {datetime.now()} Recording for 10 seconds AND Done:')
--- test_video_capture.py
+++ test_video_capture.py
@@ -5,7 +5,7 @@ picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
-output = "test.mp4"
+output = "test.h264"
 picam2.start_recording(encoder, output, quality=Quality.HIGH)
 time.sleep(10)
 picam2.stop_recording()
